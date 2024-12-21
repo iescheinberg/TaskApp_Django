d2c3f96a061fd0a660774b4009afdf61b2d7269c
iescheinberg <agr.scheinberg@gmail.com>
2024-12-20 22:10:17 -0300

    funcionalidades basicas terminadas

--- apps/task/urls.py
+++ apps/task/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
-    
+    path('agregar/', views.agregar_tarea, name='agregar_tarea'),
+    path('borrar/<int:task_id>', views.borrar_tarea, name='borrar_tarea'),
 ]
--- apps/task/views.py
+++ apps/task/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TaskForm
 from .models import Task
 # Create your views here.
 
+# Lista de tareas
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'task/task_list.html', {'tasks': tasks})
+
+
+# Borrar tarea
+def agregar_tarea(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+        
+    return render(request, 'task/agregar_tarea.html', {'form': form})
+
+
+
+# Borrar tarea
+def borrar_tarea(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
 
